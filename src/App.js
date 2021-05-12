ec0164f1ec15dec5b413380c8aeab397f7c03cbd
unknown <geo.koshy89@gmail.com>
2021-05-12 04:28:08 +0000

    Added travis deploy config

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there George!</p>
+        <p>Bye there George from travis deploy!</p>
         <a
           className="App-link"
           href="https://reactjs.org"

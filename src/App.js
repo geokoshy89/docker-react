0ece5b7d2212780115199333845c874e66851eb4
unknown <geo.koshy89@gmail.com>
2021-05-12 04:50:42 +0000

    Added change for pr merge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there George from travis deploy!</p>
+        <p>This is a CI CD via pr merge!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
